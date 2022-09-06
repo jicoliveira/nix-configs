e765ac22438a6f687540711c9282b92026afb03f
Júlio Casemiro <casemisto@gmail.com>
2022-09-06 17:32:07 -0300

    changed inputs

--- flake.nix
+++ flake.nix
@@ -4,8 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hardware.url = "github:nixos/nixos-hardware";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
@@ -13,7 +15,9 @@
       nixos = nixpkgs.lib.nixosSystem {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         specialArgs = { inherit inputs; };
-        modules = [ ./nixos/configuration.nix ];
+        modules = [ 
+          ./nixos/configuration.nix 
+        ];
       };
     };
 
@@ -22,7 +26,9 @@
         home-manager.lib.homeManagerConfiguration {
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
           extraSpecialArgs = { inherit inputs; }; 
-          modules = [ ./home-manager/home.nix ];
+          modules = [ 
+            ./home-manager/home.nix 
+          ];
         };
     };
   };
