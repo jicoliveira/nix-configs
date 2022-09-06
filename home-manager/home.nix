7dda477c1ad9d7c5d0c00ea2fa02017ca3db4b5e
Júlio Casemiro <casemisto@gmail.com>
2022-09-06 16:14:30 -0300

    add sway

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -9,6 +9,41 @@
     homeDirectory = "/home/julio";
   };
 
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = false;
+  services.xserver.desktopManager.plasma5.enable = true;
+  
+
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      wf-recorder
+      mako # notification daemon
+      grim
+     #kanshi
+      slurp
+      alacritty # Alacritty is the default terminal in the config
+      dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+  };
+
+  programs.waybar.enable = true;
+
+  # QT
+  programs.qt5ct.enable = true;
+
+
   home.packages = with pkgs; [ 
     bottom
     vlc
