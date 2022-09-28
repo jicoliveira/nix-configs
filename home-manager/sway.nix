f08a8c62f61f7c4bfae4e73ae5efd0c88dd3e8d8
Júlio Casemiro <casemisto@gmail.com>
2022-09-27 23:01:24 -0300

    add volume controls

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -36,6 +36,11 @@
 				# brightness
 				"XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl s +10%";
 				"XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl s 10%-";
+
+        # volume
+        "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
 			};
 		};
   };
