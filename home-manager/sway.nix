495c31d77b698abd0eb54ea13d3026f985e402cb
Júlio Casemiro <casemisto@gmail.com>
2022-10-15 16:31:03 -0300

    fix modifier

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -18,7 +18,7 @@
       enable = true;
       wrapperFeatures.gtk = true;
       config = {
-        inherit modifier;
+        modifier = modifier;
         terminal = "${config.programs.alacritty.package}/bin/alacritty";
           bars = [{
             command = "waybar";
@@ -50,55 +50,55 @@
           "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
 
           # screenshot
-          "${mod}+p" = "exec grimshot copy area";
-          "${mod}+Shift+p" = "exec grimshot copy active";
-          "${mod}+Ctrl+p" = "exec grimshot copy window";
+          "${modifier}+p" = "exec grimshot copy area";
+          "${modifier}+Shift+p" = "exec grimshot copy active";
+          "${modifier}+Ctrl+p" = "exec grimshot copy window";
 
           # general bindings
-          "${mod}+d" = "exec ${menu}";
-          "${mod}+Return" = "exec ${term}";
-          "${mod}+Shift+q" = "kill";
-          "${mod}+Shift+c" = "reload";
-          "${mod}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'";
+          "${modifier}+d" = "exec ${menu}";
+          "${modifier}+Return" = "exec ${term}";
+          "${modifier}+Shift+q" = "kill";
+          "${modifier}+Shift+c" = "reload";
+          "${modifier}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -B 'Yes, exit sway' 'swaymsg exit'";
           
           # move your focus around
-          "${mod}+${left}" = "focus left";
-          "${mod}+${down}" = "focus down";
-          "${mod}+${up}" = "focus up";
-          "${mod}+${right}" = "focus right";
-          "${mod}+Shift+${left}" = "move left";
-          "${mod}+Shift+${down}" = "move down";
-          "${mod}+Shift+${up}" = "move up";
-          "${mod}+Shift+${right} " = "move right";
-          "${mod}+1" = "workspace number 1";
-          "${mod}+2" = "workspace number 2";
-          "${mod}+3" = "workspace number 3";
-          "${mod}+4" = "workspace number 4";
-          "${mod}+5" = "workspace number 5";
-          "${mod}+6" = "workspace number 6";
-          "${mod}+7" = "workspace number 7";
-          "${mod}+8" = "workspace number 8";
-          "${mod}+9" = "workspace number 9";
-          "${mod}+0" = "workspace number 10";
-          "${mod}+Shift+1" = "move container to workspace number 1";
-          "${mod}+Shift+2" = "move container to workspace number 2";
-          "${mod}+Shift+3" = "move container to workspace number 3";
-          "${mod}+Shift+4" = "move container to workspace number 4";
-          "${mod}+Shift+5" = "move container to workspace number 5";
-          "${mod}+Shift+6" = "move container to workspace number 6";
-          "${mod}+Shift+7" = "move container to workspace number 7";
-          "${mod}+Shift+8" = "move container to workspace number 8";
-          "${mod}+Shift+9" = "move container to workspace number 9";
-          "${mod}+Shift+0" = "move container to workspace number 10";
+          "${modifier}+${left}" = "focus left";
+          "${modifier}+${down}" = "focus down";
+          "${modifier}+${up}" = "focus up";
+          "${modifier}+${right}" = "focus right";
+          "${modifier}+Shift+${left}" = "move left";
+          "${modifier}+Shift+${down}" = "move down";
+          "${modifier}+Shift+${up}" = "move up";
+          "${modifier}+Shift+${right} " = "move right";
+          "${modifier}+1" = "workspace number 1";
+          "${modifier}+2" = "workspace number 2";
+          "${modifier}+3" = "workspace number 3";
+          "${modifier}+4" = "workspace number 4";
+          "${modifier}+5" = "workspace number 5";
+          "${modifier}+6" = "workspace number 6";
+          "${modifier}+7" = "workspace number 7";
+          "${modifier}+8" = "workspace number 8";
+          "${modifier}+9" = "workspace number 9";
+          "${modifier}+0" = "workspace number 10";
+          "${modifier}+Shift+1" = "move container to workspace number 1";
+          "${modifier}+Shift+2" = "move container to workspace number 2";
+          "${modifier}+Shift+3" = "move container to workspace number 3";
+          "${modifier}+Shift+4" = "move container to workspace number 4";
+          "${modifier}+Shift+5" = "move container to workspace number 5";
+          "${modifier}+Shift+6" = "move container to workspace number 6";
+          "${modifier}+Shift+7" = "move container to workspace number 7";
+          "${modifier}+Shift+8" = "move container to workspace number 8";
+          "${modifier}+Shift+9" = "move container to workspace number 9";
+          "${modifier}+Shift+0" = "move container to workspace number 10";
           
           # layout stuff
-          "${mod}+s" = "layout stacking";
-          "${mod}+t" = "layout tabbed";
-          "${mod}+e" = "layout toggle split";
-          "${mod}+f" = "floating toggle";
-          "${mod}+Shift+r" = "mode resize";
-          "${mod}+v" = "split v";
-          "${mod}+h" = "split h";
+          "${modifier}+s" = "layout stacking";
+          "${modifier}+t" = "layout tabbed";
+          "${modifier}+e" = "layout toggle split";
+          "${modifier}+f" = "floating toggle";
+          "${modifier}+Shift+r" = "mode resize";
+          "${modifier}+v" = "split v";
+          "${modifier}+h" = "split h";
 
         };
       };
