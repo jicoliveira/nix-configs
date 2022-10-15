aba02f3dff400116944fea65a640ad49010de6a0
Júlio Casemiro <casemisto@gmail.com>
2022-10-15 18:13:19 -0300

    add configurations from waybar wiki

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -2,24 +2,91 @@
 {
   programs.waybar = {
     enable = true;
-    settings = {
-      layer = "top";
-      modules-left = ["sway/workspaces" "sway/mode"];
-      modules-center = ["sway/window"];
-      modules-right = ["battery" "clock"];
-      "sway/window" = {
-          max-length = 50;
-      };
-      battery = {
-          format = "{capacity}% {icon}";
-          format-icons = [ "" "" "" "" "" ];
-          format-charging = "{capacity}% ";
-          format-plugged = "{capacity}% ";
-      };
-      clock = {
-          format-alt = "{:%a, %d. %b  %H:%M}";
-      };
-    };
+    settings = [ 
+      {
+        layer = "top";
+        modules-left = ["sway/workspaces" "sway/mode" "sway/window"];
+        modules-center = [];
+        modules-right = ["idle_inhibitor" "pulseaudio" "network" "cpu" "memory" "temperature" "backlight" "keyboard-state" "battery" "clock" "tray"];
+    
+        "sway/window" = {
+            max-length = 50;
+        };
+
+        "sway/mode" = {
+          format = "<span style=\"italic\">{}</span>";
+        };
+
+        idle_inhibitor = {
+          format = "{icon}";
+          format-icons = {
+              activated = "";
+              deactivated = "";
+          };
+        };
+
+        battery = {
+            format = "{capacity}% {icon}";
+            format-icons = [ "" "" "" "" "" ];
+            format-charging = "{capacity}% ";
+            format-plugged = "{capacity}% ";
+        };
+
+        clock = {
+            format-alt = "{:%a; %d. %b  %H:%M}";
+            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
+        
+        cpu = {
+            format = "{usage}% ";
+        };
+
+        memory = {
+            format = "{}% ";
+        };
+
+        temperature = {
+            thermal-zone = 2;
+            critical-threshold = 80;
+            format = "{temperatureC}°C {icon}";
+            format-icons = ["" "" ""];
+        };
+        
+        backlight = {
+            format = "{percent}% {icon}";
+            format-icons = ["" "" "" "" "" "" "" "" ""];
+        };
+
+        network = {
+            format-wifi = "{essid} ({signalStrength}%) ";
+            format-ethernet = "{ipaddr}/{cidr} ";
+            tooltip-format = "{ifname} via {gwaddr} ";
+            format-linked =  "{ifname} (No IP) ";
+            format-disconnected = "Disconnected ⚠";
+            format-alt = "{ifname}: {ipaddr}/{cidr}";
+        };
+
+        pulseaudio = {
+            scroll-step = 5;
+            format = "{volume}% {icon} {format_source}";
+            format-bluetooth = "{volume}% {icon} {format_source}";
+            format-bluetooth-muted = " {icon} {format_source}";
+            format-muted = " {format_source}";
+            format-source = "{volume}% ";
+            format-source-muted = "";
+            format-icons = {
+                headphone = "";
+                hands-free = "";
+                headset = "";
+                phone = "";
+                portable = "";
+                car = "";
+                default = ["" "" ""];
+            };
+            on-click = "pavucontrol";
+        };
+      } 
+    ];
   };
 }
  
