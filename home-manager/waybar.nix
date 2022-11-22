98b076ada64d1611135afa5d641d7d905cb8e691
Julio I. C. Oliveira <casemisto@gmail.com>
2022-11-22 19:29:53 -0300

    add style to swaybar

--- home-manager/waybar.nix
+++ home-manager/waybar.nix
@@ -2,6 +2,20 @@
 {
   programs.waybar = {
     enable = true;
+    style = ''
+      * {
+        border: none;
+        border-radius: 0;
+        font-family: Fira Code;
+      }
+      window#waybar {
+        background: #16191C;
+        color: #AAB2BF;
+      }
+      #workspaces button {
+        padding: 0 5px;
+      }
+      '';
     settings = [ 
       {
         layer = "top";
