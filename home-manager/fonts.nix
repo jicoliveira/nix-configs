07a789306bd3a59e11b71cb6836e02d8d9aeb59f
Julio I. C. Oliveira <casemisto@gmail.com>
2023-01-04 16:50:48 -0300

    add fonts

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -2,6 +2,11 @@
   fonts.fontconfig.enable = true;
   home.packages = [
     pkgs.fira
+    pkgs.source-sans
+    pkgs.aurulent-sans
+    pkgs.lato
+    pkgs.liberation_ttf
+    pkgs.montserrat
     (pkgs.nerdfonts.override { fonts = [ "FiraCode"]; })
   ];
 }
